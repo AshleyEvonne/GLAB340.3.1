63122fcd353af3eb6f42fe0b2cc74bc19a38113e
AshleyEvonne <ashleyevonne345@gmail.com>
2024-10-08 16:32:15 -0400

    working with strings in python

---
+++ Strings.py
@@ -0,0 +1,44 @@
+# Word Play game
+word = input("Enter a word: ")
+
+#Length of the word
+print("Length of the word: ", len(word))
+
+#Uppercase and  Lowercase
+print("Uppercase:", word.upper())
+print("Lowercase:", word.lower())
+
+#Counting occurrences
+letter = input("Enter a Letter: ")
+print(f"'{letter}' appears {word.count(letter)} times(s) in '{word}'")
+
+substring = input("Enter a substring:")
+print(f"'{substring} appears {word.count(substring)} times(s) in '{word}'")
+
+#Revering the word
+reverse_word = word[::-1]
+print("Reverse:", reverse_word)
+
+#Slicing the word
+start_index = int(input("Enter a starting index: "))
+end_index = int(input("Enter an ending index: "))
+sliced_word = word[start_index:end_index]
+print("sliced word:", sliced_word)
+
+#Replacing a character(
+char_to_replace = input("Enter a second word: ")
+replacement_char = input("Enter the replacement character: ")
+new_word = word.replace(char_to_replace, replacement_char, 1)
+print("Replaced:", new_word)
+
+#Concatenating with another word
+second_word = input("Enter a second word: ")
+concatenated_word = word + second_word
+print("Concatenated:", concatenated_word)
+
+#Checking for palindrome and valid identifier
+is_palindrome = word == reverse_word
+is_valid_identifier = word.isidentifier()
+
+print("Is a plaindrome?", is_palindrome)
+print("Is a valid Python identifier?", is_valid_identifier)
